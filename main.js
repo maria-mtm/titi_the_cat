ce28e4a46fba76da952cae55fbd5e0af404ec2fe
melendezcabreram <melendezcabreram@gmail.com>
2023-05-21 20:59:30 +0200

    finished

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const menuNav = document.querySelector('#nav')
+const openMenu = document.querySelector('#toOpenBtn')
+const closeMenu = document.querySelector('#toCloseBtn')
+
+openMenu.addEventListener("click", () =>{
+    menuNav.classList.add("visible");
+})
+
+closeMenu.addEventListener("click", () =>{
+    menuNav.classList.remove("visible");
+})
+
+
+
+
 
 // ACORDEON
 
